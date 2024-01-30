457f722285f521a08853f2f96508f729b8600745
Ethan112358 <acuratnam@gmail.com>
2024-01-30 22:28:15 +0800

    Lecture 29~33
    Lecture 29. Plate Complete Visual
    Lecture 30. Plate World UI Icons
    Lecture 31. Delivery Counter, Shader Graph
    Lecture 32. Delivery Manager
    Lecture 33. Delivery Manager UI

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject {
+
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    }
 
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
@@ -25,8 +32,17 @@ public class PlateKitchenObject : KitchenObject {
             return false;
         } else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+                kitchenObjectSO = kitchenObjectSO
+            });
+
             return true;
         }
+    }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList() {
+        return kitchenObjectSOList;
     }
 
 }
